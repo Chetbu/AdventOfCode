9e5ffa477e18d8030588008b2df8bb2fc5c76f77
Fabien Chazal <fabien.chazal@gmail.com>
2025-12-04 09:25:44 +0100

    day 4

---
+++ 2025/4-12/solution_1.py
@@ -0,0 +1,61 @@
+
+import time
+from pathlib import Path
+
+script_name = Path(__file__).stem
+current_dir = str(Path(__file__).parent)
+
+
+file_filepath = Path(current_dir, "data.txt")
+start_time = time.time()
+
+possible_moves_array = []
+for i in range(-1,2,1):
+    for j in range(-1,2,1):
+        possible_moves_array.append((i,j))
+possible_moves_array.remove((0,0))
+# print(possible_moves_array)
+
+def count_adjacent_rolls(t, map):
+    count = 0
+    for m in possible_moves_array:
+        new_t = (t[0] + m[0], t[1] + m[1])
+        if new_t[0] < 0 or new_t[1] < 0 or new_t[0] >= len(map) or new_t[1] >= len(map[0]):
+            #coordonate outside the map, considered as a space
+            continue
+        elif map[new_t[0]][new_t[1]] == "@":
+            count += 1
+    return count
+
+
+
+with open(file_filepath) as f:
+    lines = f.read().splitlines()
+
+# print(lines)
+# print(count_adjacent_rolls((0,2),lines))
+
+result = 0
+i = 0
+while i < len(lines):
+    j = 0
+    while j < len(lines[i]):
+        if lines[i][j] == "@":
+            count = count_adjacent_rolls((i,j),lines)
+            if count <= 3:
+                result += 1
+        j += 1
+    i += 1
+
+print(result)
+
+
+
+    
+
+
+
+
+
+
+ 
